aad3770b58fcf4d491b651f18e7732aac044c866
Petr Kundrata <pksoft@atlas.cz>
2020-10-31 00:32:46 +0100

    [MISC] sha1 functions ...

--- src/missings.cpp
+++ src/missings.cpp
@@ -104,15 +104,7 @@ void mergesort_r2(void *base, int nmemb, int size,
 
 //-------------------------------------------------------------------------------------------------
 // https://github.com/git/git/blob/master/block-sha1/sha1.h
-typedef struct {
-    unsigned long long size;
-    unsigned int H[5];
-    unsigned int W[16];
-} blk_SHA_CTX;
 
-void blk_SHA1_Init(blk_SHA_CTX *ctx);
-void blk_SHA1_Update(blk_SHA_CTX *ctx, const void *dataIn, unsigned long len);
-void blk_SHA1_Final(unsigned char hashout[20], blk_SHA_CTX *ctx);
 
 #define platform_SHA_CTX    blk_SHA_CTX
 #define platform_SHA1_Init  blk_SHA1_Init
