f283c3c0e48d810479d781e1c5bf88ea2a97203f
Petr Kundrata <pksoft@atlas.cz>
2026-04-15 23:44:56 +0200

    [WIP] IPv4TO4U()

--- src/bin_xml_types.cpp
+++ src/bin_xml_types.cpp
@@ -908,7 +908,7 @@ int XBT_ToStringChunk(XML_Binary_Type type,const char *data,int &offset,char *ds
                 uint32_t ip_addr = *reinterpret_cast<const uint32_t*>(data);
 
 
-                snprintf(dst,dst_size,"%u.%u.%u.%u",ip_addr >> 24,(ip_addr >> 16) & 0xff,(ip_addr >> 8) & 0xff,ip_addr & 0xff);
+                snprintf(dst,dst_size,"%u.%u.%u.%u",IPv4TO4U(ip_addr));//ip_addr >> 24,(ip_addr >> 16) & 0xff,(ip_addr >> 8) & 0xff,ip_addr & 0xff);
                 return strlen(dst);
             } 
         case XBT_INT64:
